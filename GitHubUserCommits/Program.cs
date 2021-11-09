d770f03ffaab6858b55e745d9c70db210d5cabb7
vdburle <93744836+vdburle@users.noreply.github.com>
2021-11-09 05:34:53 -0800

    Add files via upload

--- GitHubUserCommits/Program.cs
+++ GitHubUserCommits/Program.cs
@@ -11,14 +11,19 @@ namespace GitHubUserCommits
 {
     class Program
     {
+        //Constant definitions for github url and the commit search API
         private const string baseURL = "https://api.github.com/";
         private const string searchAPIParams = "/search/commits?q=committer:{0}&sort=committer-date&order=desc&per_page={1}&page={2}";
+
+        //Constant definitions for fetching number of results per page and the PageNumber to fetch. Handy when retrieving using pagination.
         private const string perPageResults = "60";
         private const string pageNumtoFecth = "1";
 
+        //global variables for reading and passing user input, the commiter name and the authentication token
         public static string commiter;
         public static string token;
 
+        //Internal class to deserialize Committer Json from the results
         internal class Committer
         {
             public DateTime date { get; set; }
@@ -27,6 +32,7 @@ namespace GitHubUserCommits
             public string login { get; set; }
         }
 
+        //Internal class to deserialize Commits Json from the results
         internal class Commit
         {
             public string url { get; set; }
@@ -35,11 +41,13 @@ namespace GitHubUserCommits
             public int comment_count { get; set; }
         }
 
+        //Internal class to deserialize search results Json from the results
         internal class Item
         {
             public Commit commit { get; set; }
         }
 
+        //Internal class to deserialize Root Json from the results
         internal class Root
         {
             public int total_count { get; set; }
@@ -47,18 +55,21 @@ namespace GitHubUserCommits
             public List<Item> items { get; set; }
         }
 
+        //Main Method
         static void Main(string[] args)
         {
 
 
-            getUserInput();
-            Root responseData = fetchCommitDataAsync().Result;
-            writeToCSV(responseData);
+            getUserInput(); //read user input
+            Root responseData = fetchCommitDataAsync().Result; //retrieve user commits using github search commits api
+            writeToCSV(responseData); //write the results to CSV and display mean time between commits
 
 
         }
 
-        public static void getUserInput()
+        //Method to read user input
+        //the method prompts input until a non empty value is input for username and token
+        private static void getUserInput()
         {
             while (true)
             {
@@ -86,6 +97,7 @@ namespace GitHubUserCommits
             }
         }
 
+        //Method to fetch the latest 60 user commits using github API
         public static async Task<Root> fetchCommitDataAsync()
         {
             Root responseData = null;
@@ -93,33 +105,37 @@ namespace GitHubUserCommits
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri(baseURL);
+
+                //construct the httprequest header
                 client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("GHApp_vdburle", "1"));
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.cloak-preview"));
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("token", token);
 
-                using var response = await client.GetAsync(string.Format(searchAPIParams, commiter, perPageResults, pageNumtoFecth));
-
-                try
+                using (var response = await client.GetAsync(string.Format(searchAPIParams, commiter, perPageResults, pageNumtoFecth)))
                 {
-                    response.EnsureSuccessStatusCode();
-                    responseData = response.Content.ReadFromJsonAsync<Root>().Result;
 
-                    if (responseData.items.Count == 0)
+                    try
                     {
-                        throw new Exception(string.Format("The user {0} doesnt have any commits", commiter));
+                        response.EnsureSuccessStatusCode();
+                        responseData = response.Content.ReadFromJsonAsync<Root>().Result;
+
+                        if (responseData.items.Count == 0)
+                        {
+                            throw new Exception(string.Format("The user {0} doesnt have any commits", commiter));
+                        }
                     }
-                }
-                catch (HttpRequestException ex)
-                {
-                    Console.WriteLine("There is a problem with the API response, please check the username and token");
-                    Console.WriteLine(ex.Message);
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Environment.Exit(0);
+                    catch (HttpRequestException ex)
+                    {
+                        Console.WriteLine("There is a problem with the API response, please check the username and token");
+                        Console.WriteLine(ex.Message);
+                        Environment.Exit(0);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Environment.Exit(0);
+                    }
                 }
             }
 
@@ -147,11 +163,13 @@ namespace GitHubUserCommits
                 }
                 avgCommitInSec = cumulativeCheckinTS / (numCommitResults - 1);
                 File.WriteAllText(commiter + "_commits.csv", sb.ToString());
-                Console.WriteLine("Average time between commits for the user {0} is : {1}", commiter, avgCommitInSec);
+                                
+                Console.WriteLine("Average time between commits for the user {0} is : {1:%d} days {1:%h} hours {1:%m} minutes {1:%s} seconds", commiter, TimeSpan.FromSeconds(avgCommitInSec));
+                
             }
         }
 
-        
+
 
     }
 }
